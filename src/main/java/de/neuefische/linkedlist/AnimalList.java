3ee2d0193e724b58cb24c3d6cf3c9b7f684db0a6
Jan Robert <janmartinrobert@gmail.com>
2021-07-21 17:04:21 +0200

    Implement toString and add

--- src/main/java/de/neuefische/linkedlist/AnimalList.java
+++ src/main/java/de/neuefische/linkedlist/AnimalList.java
@@ -2,6 +2,35 @@ package de.neuefische.linkedlist;
 
 public class AnimalList {
 
-    public void add(Animal animal) {}
+    private AnimalListItem head;
+
+    public void add(Animal animal) {
+        if(head == null){
+            head = new AnimalListItem(animal);
+        }else {
+            AnimalListItem current = head;
+            while(current.getNext() != null){
+                current = current.getNext();
+            }
+            current.setNext(new AnimalListItem(animal));
+        }
+    }
+
+    @Override
+    public String toString(){
+        if(head == null){
+            return "";
+        }
+
+        StringBuilder builder = new StringBuilder(head.getValue().getName());
+
+        AnimalListItem current = head.getNext();
+        while(current != null){
+            builder.append(" -> ").append(current.getValue().getName());
+            current = current.getNext();
+        }
+
+        return builder.toString();
+    }
 
 }
--- src/main/java/de/neuefische/linkedlist/AnimalListItem.java
+++ src/main/java/de/neuefische/linkedlist/AnimalListItem.java
@@ -5,4 +5,23 @@ public class AnimalListItem {
     private Animal value;
     private AnimalListItem next;
 
+    public AnimalListItem(Animal animal) {
+        this.value = animal;
+    }
+
+    public Animal getValue() {
+        return value;
+    }
+
+    public void setValue(Animal value) {
+        this.value = value;
+    }
+
+    public AnimalListItem getNext() {
+        return next;
+    }
+
+    public void setNext(AnimalListItem next) {
+        this.next = next;
+    }
 }
