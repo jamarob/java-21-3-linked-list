7e7f0f094ab59b7f7da10e7a7e81956883b6a4ae
Jan Robert <janmartinrobert@gmail.com>
2021-07-21 18:08:36 +0200

    Add tests for remove

--- src/test/java/de/neuefische/linkedlist/AnimalListTest.java
+++ src/test/java/de/neuefische/linkedlist/AnimalListTest.java
@@ -1,6 +1,10 @@
 package de.neuefische.linkedlist;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -37,4 +41,31 @@ class AnimalListTest {
         assertEquals(expected, actual);
     }
 
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("provideTestRemoveArguments")
+    public void testRemoveItemsFromList(String description, String[] animalNames, String animalNameToRemove, String expected){
+        // GIVEN
+        AnimalList list = new AnimalList();
+        for(String animalName: animalNames){
+            list.add(new Animal(animalName));
+        }
+
+        // WHEN
+        list.remove(new Animal(animalNameToRemove));
+        String actual = list.toString();
+
+        // THEN
+        assertEquals(expected, actual);
+    }
+
+    private static Arguments[] provideTestRemoveArguments(){
+        return new Arguments[]{
+                Arguments.of("Remove only element", new String[]{"dog"}, "dog", ""),
+                Arguments.of("Remove 1 from  head", new String[]{"dog","cat","mouse"}, "dog", "cat -> mouse"),
+                Arguments.of("Remove 1 from middle",new String[]{"dog","cat","mouse"}, "cat", "dog -> mouse"),
+                Arguments.of("Remove 1 from last", new String[]{"dog","cat","mouse"}, "mouse", "dog -> cat"),
+                Arguments.of("Remove 2 consecutive",new String[]{"cat","dog","dog","cat","mouse"}, "dog", "cat -> cat -> mouse"),
+                Arguments.of("Remove 2 from head",new String[]{"dog","dog","cat","dog","mouse","dog"}, "dog", "cat -> mouse"),
+        };
+    }
 }
