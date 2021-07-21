78f22d4c6bbc1928f6b32ebb646a2b968a4276cb
Jan Robert <janmartinrobert@gmail.com>
2021-07-21 15:19:39 +0200

    Add tests for add and toString

---
+++ src/main/java/de/neuefische/linkedlist/AnimalList.java
@@ -0,0 +1,9 @@
+package de.neuefische.linkedlist;
+
+public class AnimalList {
+
+    public AnimalList add(Animal dog) {
+        return this;
+    }
+
+}
---
+++ src/test/java/de/neuefische/linkedlist/AnimalListTest.java
@@ -0,0 +1,41 @@
+package de.neuefische.linkedlist;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AnimalListTest {
+
+    @Test
+    public void toStringOfEmptyListReturnsEmptyString(){
+        // GIVEN
+        AnimalList list = new AnimalList();
+
+        // WHEN
+        String actual = list.toString();
+
+        // THEN
+        assertEquals("", actual);
+    }
+
+    @Test
+    public void testAddToList(){
+        // GIVEN
+        Animal dog = new Animal("dog");
+        Animal cat = new Animal("cat");
+        Animal mouse = new Animal("mouse");
+        AnimalList list = new AnimalList();
+
+        // WHEN
+        list
+                .add(dog)
+                .add(cat)
+                .add(mouse);
+        String actual = list.toString();
+
+        // THEN
+        String expected = "dog -> cat -> mouse";
+        assertEquals(expected, actual);
+    }
+
+}
